3f9dc2a58dd052faced38b502530a8e3caece4f4
Sergio Martín <sergio.mare2002@gmail.com>
2023-09-03 17:35:21 +0200

    Background & Tiles

---
+++ public/js/loaders.js
@@ -0,0 +1,16 @@
+// La función resolve no se llama hasta que se dispara el evento load. Esto sucede después de que se haya establecido la propiedad src y la imagen se haya cargado completamente.
+// Por lo tanto, la promesa no se resuelve hasta que la imagen se haya cargado completamente.
+export function loadImage(url) {
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', () => {
+            resolve(image);
+        });
+        image.src = url;
+    });
+}
+
+export function loadLevel(name) {
+    return fetch(`/levels/${name}.json`)
+    .then(r => r.json());
+}
