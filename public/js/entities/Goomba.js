7cbe6f837471be32527a37304202c9158e4eff27
Sergio Martín <sergio.mare2002@gmail.com>
2023-10-10 02:15:19 +0200

    Entity Interaction

--- public/js/entities/Goomba.js
+++ public/js/entities/Goomba.js
@@ -1,5 +1,6 @@
-import Entity, {Sides} from "../Entity.js";
-import PendulumWalk from "../traits/PendulumWalk.js"; 
+import Entity, {Sides, Trait} from "../Entity.js";
+import PendulumMove from "../traits/PendulumMove.js"; 
+import Killable from "../traits/Killable.js";
 import { loadSpriteSheet } from '../loaders.js';
 
 // Load functions are asynchronous and create functions are synchronous.
@@ -8,20 +9,50 @@ export function loadGoomba() {
         .then(createGoombaFactory);
 }
 
+class Behavior extends Trait {
+    constructor() {
+        super('behavior');
+    }
+
+    collides(us, them) {
+        if (us.killable.dead) {
+            return;
+        }
+        // Feature detection
+        if (them.stomper) {
+            if (them.vel.y > us.vel.y) {
+                us.killable.kill();
+                us.pendulumMove.speed = 0;
+            } else { // El goomba hace daño a mario (Stomper)
+                them.killable.kill();
+            }
+        }
+    }
+}
+
 function createGoombaFactory(sprite) {
     // Scope de createGoombaFactory
 
     const walkAnim = sprite.animations.get('walk');
 
+    function routeAnim(goomba) {
+        if (goomba.killable.dead) {
+            return 'flat'
+        }
+        return walkAnim(goomba.lifetime);
+    }
+
     function drawGoomba(context) {
-        sprite.draw(walkAnim(this.lifetime), context, 0, 0);
+        sprite.draw(routeAnim(this), context, 0, 0);
     }
 
     return function createGoomba() {
         const goomba = new Entity();
         goomba.size.set(16, 16);
 
-        goomba.addTrait(new PendulumWalk());
+        goomba.addTrait(new PendulumMove());
+        goomba.addTrait(new Behavior());
+        goomba.addTrait(new Killable());
 
         goomba.draw = drawGoomba;
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,22 @@
 import Camera from './Camera.js';
+import Entity from './Entity.js';
+import PlayerController from './traits/PlayerController.js';
 import Timer from './Timer.js';
 import { createLevelLoader } from './loaders/level.js';
 import { loadEntities } from './entities.js';
 import { createCollisionLayer, createCamaraLayer } from './layers.js';
 import { setupKeyboard } from './input.js';
 import { setupMouseControl } from './debug.js';
+
+function createPlayerEnv(playerEntity) {
+    const playerEnv = new Entity();
+    const playerControl = new PlayerController();
+    playerControl.checkpoint.set(64, 64);
+    playerControl.setPlayer(playerEntity);
+    playerEnv.addTrait(playerControl);
+    
+    return playerEnv;
+}
 
 async function main(canvas) {
 
@@ -17,9 +29,15 @@ async function main(canvas) {
 
     const camera = new Camera();
 
+    // Comentar si queremos desactivar el collision layer
+    level.comp.layers.push(
+        createCollisionLayer(level),
+        /* createCamaraLayer(camera) */);
+
     const mario = entityFactory.mario();
-    mario.pos.set(64, 64);
-    level.entities.add(mario);
+
+    const playerEnv = createPlayerEnv(mario);
+    level.entities.add(playerEnv);
 
     const input = setupKeyboard(mario);
     input.listenTo(window);
@@ -31,9 +49,7 @@ async function main(canvas) {
     timer.update = function update(deltaTime) {
             level.update(deltaTime);
 
-            if (mario.pos.x > 100) {
-                camera.pos.x = mario.pos.x - 100;
-            }
+            camera.pos.x = Math.max(0, mario.pos.x - 100);
 
             level.comp.draw(context, camera);
     }
